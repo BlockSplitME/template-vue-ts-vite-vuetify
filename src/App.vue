358b08ee4feaf587a17566f27647017a16a05cd1
Евгений Белов <j.belov@users-MacBook-Pro.local>
2024-04-18 14:37:40 +0300

    template

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Message :msg="'Hello'"/>
+</template>
+
+<script setup lang="ts">
+  import Message from "./components/Message.vue";
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <v-card-title> {{msg}}</v-card-title>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+  import { defineProps } from 'vue'
+
+  defineProps<{ msg: string }>()
+
+</script>
+
+<style scoped>
+
+</style>
